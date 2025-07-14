c555ce85bf3ae17a63c937f5892b4d635fbe3176
Lin, Ming Chih <mingchih.lin8@gmail.com>
2025-07-14 20:51:27 +0800

    Improve component renaming step

--- app/flows/Flow_SIwave_SYZ/step_03.py
+++ app/flows/Flow_SIwave_SYZ/step_03.py
@@ -1,7 +1,15 @@
 import os
 import json
-from collections import defaultdict
 from pyedb import Edb
+
+
+def change_name(edb_obj, old_name, new_name):
+    """Rename a component definition inside the EDB."""
+    for comp_def in edb_obj.component_defs:
+        if comp_def.GetName() == old_name:
+            comp_def.SetName(new_name)
+            return True
+    return False
 
 
 def run(job_path, data=None, files=None):
@@ -13,14 +21,11 @@ def run(job_path, data=None, files=None):
     rename_log = {}
     edb = Edb(edb_dir, edbversion="2024.1")
     if data:
-        for comp_name, comp in edb.components.components.items():
+        for _, comp in edb.components.components.items():
             new_name = data.get(f"new_{comp.part_name}")
             if new_name and new_name != comp.part_name:
-                for comp_def in edb.component_defs:
-                    if comp_def.GetName() == comp.part_name:
-                        comp_def.SetName(new_name)
-                        rename_log[comp.part_name] = new_name
-                        break
+                if change_name(edb, comp.part_name, new_name):
+                    rename_log[comp.part_name] = new_name
         if rename_log:
             edb.save()
     edb.close_edb()
