aa80be26fa01e5e4b58682832deba681971b2e3d
Lin, Ming Chih <mingchih.lin8@gmail.com>
2025-07-14 15:19:53 +0800

    Use Waitress for running app

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0')
+if __name__ == "__main__":
+    # Use waitress for production-ready serving
+    serve(app, host="0.0.0.0", port=5000)
